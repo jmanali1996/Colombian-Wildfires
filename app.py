9b85409aa85ee980ed1925621a0e6a9324d095a8
Manali Jain <115955036+jmanali1996@users.noreply.github.com>
2024-06-29 20:21:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objs as go
 import pandas as pd
-import datashader as ds
-from colorcet import fire
-import datashader.transfer_functions as tf
 
 # Load historic wildlife data and make necessary changes
-df = pd.read_parquet('https://raw.githubusercontent.com/jmanali1996/Colombian-Wildfires/main/Data/col_2000-2024.parquet')
+df = pd.read_parquet('https://raw.githubusercontent.com/jmanali1996/Colombian-Wildfires/main/Data/col_2000-2024.parquet')#, low_memory=False)
 df['type'] = df['type'].fillna(df['type'].mode()[0])
 df = df.astype({"type": 'str'})
 df.loc[df['type'] == '0.0', 'type'] = "Presumed vegetation fire"
@@ -23,174 +20,133 @@ df = df.astype({"acq_date": 'str', "daynight": 'str'})
 df.loc[df['daynight'] == 'D', 'daynight'] = "Day time fire"
 df.loc[df['daynight'] == 'N', 'daynight'] = "Night time fire"
 df = df.rename(columns={"type": "fire origin", "acq_date": "date", "daynight": "fire time"})
+month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+df['month'] = df['month'].map(lambda x: month_order[x-1])
+unique_months = sorted(df['month'].unique(), key=lambda x: month_order.index(x))
+mo = [{'label':m,'value':m} for m in unique_months]
+yo = [{'label':y,'value':y} for y in sorted(df['year'].unique())]
 
-# Datashader image
-cvs = ds.Canvas(plot_width=1400, plot_height=700)
-agg = cvs.points(df, x="latitude", y="longitude")
-coords_lat, coords_lon = agg.coords['latitude'].values, agg.coords['longitude'].values
-coordinates = [[coords_lon[0], coords_lat[0]],
-               [coords_lon[-1], coords_lat[0]],
-               [coords_lon[-1], coords_lat[-1]], 
-               [coords_lon[0], coords_lat[-1]]]
-img = tf.shade(agg, cmap=fire, how='eq_hist')[::-1].to_pil()
-
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.UNITED])
 app.layout = dbc.Container([
     html.H1("Wildfires in Colombia", style={'text-align': 'center'}),
-    html.H5("The line graph and map showcase the pattern of fire frequency and the spread of fires from 2000-2024", style={'text-align': 'center'}),
-    html.H6("Kindly use the filters to get the desired insights."),
+    html.H5("The bar graph and map showcase the pattern of fire frequency and the spread of fires from 2000-2024", style={'text-align': 'center'}),
+    html.Br(),
+    html.Div([
     # Fire origin checklist
-    html.Label(children=['Fire origin:'], style={'color': 'white', 'font-weight': 'bold'}),
-    dcc.Checklist(
-        id='fire_origin_type',
-        options=df['fire origin'].unique(),
-        value=[fo for fo in sorted(df['fire origin'].unique())],
-        inline=True,
-        labelStyle={'margin-right': 20}
-    ),
-    html.Br(),
+        html.Div([
+            html.Label(children=['Fire origin:'], style={'color': 'black', 'font-weight': 'bold'}),
+            dcc.Checklist(
+                id='fire_origin_type',
+                options=df['fire origin'].unique(),
+                value=[fo for fo in sorted(df['fire origin'].unique())],
+                inline=True,
+                labelStyle={'margin-right': 20}
+            )
+        ], style={'display': 'inline-block', 'width': '56%', 'margin-right': 20, 'vertical-align': 'top'}),
     # Fire time checklist
-    html.Label(children=['Fire time:'], style={'color': 'white', 'font-weight': 'bold'}),
-    dcc.Checklist(
-        id='fire_time_type',
-        options=df['fire time'].unique(),
-        value=[ft for ft in sorted(df['fire time'].unique())],
-        inline=True,
-        labelStyle={'margin-right': 20}
-    ),
+        html.Div([
+            html.Label(children=['Fire time:'], style={'color': 'black', 'font-weight': 'bold'}),
+            dcc.Checklist(
+                id='fire_time_type',
+                options=df['fire time'].unique(),
+                value=[ft for ft in sorted(df['fire time'].unique())],
+                inline=True,
+                labelStyle={'margin-right': 20}
+            )
+        ], style={'display': 'inline-block', 'width': '40%', 'vertical-align': 'top'})
+    ]),
     html.Br(),
+    html.Div([
     # Months dropdown
-    html.Div([
-        html.Label(children=['Month:'], style={'color': 'white', 'font-weight': 'bold'}),
-        dcc.Dropdown(
-            id='month-variable',
-            options=[{'label':m,'value':m} for m in sorted(df['month'].unique())],
-            value=None,
-            multi=True,
-            searchable=True,
-            placeholder="Select a month"
-        ),
-    ], style={'display': 'inline-block', 'margin-right': 20, 'width': 300}),
+        html.Div([
+            html.Label(children=['Month:'], style={'color': 'black', 'font-weight': 'bold'}),
+            dcc.Dropdown(
+                id='month-variable',
+                options=mo,
+                value=None, 
+                multi=True,
+                searchable=True,
+                placeholder="Select a month"
+            ),
+        ], style={'display': 'inline-block', 'width': '56%', 'margin-right': 20, 'vertical-align': 'top'}),
     # Years dropdown
-    html.Div([
-        html.Label(children=['Year:'], style={'color': 'white', 'font-weight': 'bold'}),
-        dcc.Dropdown(
-            id='year-variable',
-            options=[{'label':y,'value':y} for y in sorted(df['year'].unique())],
-            value=None,
-            multi=True,
-            searchable=True,
-            placeholder="Select a year"
-        ),
-    ], style={'display': 'inline-block', 'margin-right': 20, 'width': 300}),
+        html.Div([
+            html.Label(children=['Year:'], style={'color': 'black', 'font-weight': 'bold'}),
+            dcc.Dropdown(
+                id='year-variable',
+                options=yo,
+                value=None,
+                multi=True,
+                searchable=True,
+                placeholder="Select a year"
+            ),
+        ], style={'display': 'inline-block', 'width': '40%', 'vertical-align': 'top'})
+    ]),
     html.Br(),
-    html.Br(),
-    # Map theme checklist
-    html.Label(children=['Map theme:'], style={'color':'white', 'font-weight': 'bold'}),
-    dcc.RadioItems(
-        id='map-theme',
-        options=[
-            {'label': 'Light Theme', 'value': 'light'},
-            {'label': 'Dark Theme', 'value': 'dark'}
-        ],
-        value='light',  
-        inline=True,
-        labelStyle={'margin-right': 20}
-    ),
-    html.Br(),
-    dbc.Button(id='fire-button', children="Submit"),
+    dbc.Button(id='fire-button', children="Submit", n_clicks=0),
     html.Br(),
     html.Br(),
     # Fire instances count
-    html.Div(id='count-output', style={'color': 'white', 'font-weight': 'bold'}),
+    html.Div(id='count-output', style={'color': 'black', 'font-weight': 'bold'}),
     html.Br(),
-    dbc.Row(
-        [
-            dbc.Col(dcc.Graph(id='bar'))
-        ]
-    ),
-    html.Br(),
-    dbc.Row(
-        [
-            dbc.Col(dcc.Graph(id='graph'))
-        ]
-    ),
+    html.Div(id='placeholder', children="Select your preferences to view the bar graph and map"),
+    dbc.Row([dbc.Col(dcc.Graph(id='bar'))]),
+    dbc.Row([dbc.Col(dcc.Graph(id='map'))]),
+], style={'margin-left': 10})
 
-], style={'margin-left': 20})
-
-
-# Getting count and bar chart
+# Getting count and bar graph and map
 @app.callback(
-    Output('count-output', 'children'),
+    [Output('count-output', 'children'), 
     Output('bar', 'figure'),
-    Input('fire-button', 'n_clicks'),
-    State('fire_origin_type', 'value'),
+    Output('map', 'figure'),
+    Output('placeholder', 'children')],
+    [Input('fire-button', 'n_clicks')],
+    [State('fire_origin_type', 'value'),
     State('fire_time_type', 'value'),
     State('month-variable', 'value'),
-    State('year-variable', 'value'),
+    State('year-variable', 'value')],
     prevent_initial_call=True
 )
-def update_bar(_, selected_category, selected_value, selected_month, selected_year):
-    if selected_month and selected_year:
+def update_bar(n_clicks, selected_category, selected_value, selected_month, selected_year):
+    if n_clicks > 0 and selected_category and selected_value and selected_month and selected_year:
         df_sub = df[(df['fire origin'].isin(selected_category)) & (df['fire time'].isin(selected_value)) & (df['month'].isin(selected_month)) & (df['year'].isin(selected_year))]
-    else:
-        df_sub = df[(df['fire origin'].isin(selected_category)) & (df['fire time'].isin(selected_value))]
-    count = df_sub.shape[0]
-    df_sub = df_sub.groupby(["date","fire origin"])["brightness"].mean().reset_index()
-    print(df_sub)
-    fig_bar = px.bar(
-        df_sub,
-        x="date",
-        y="brightness",
-        labels={"brightness": "fire temperature (K)"},
-        color="fire origin",
-        barmode='group',
-        title=f"Bar Chart for {selected_value} {selected_month} {selected_year}",
-        width=1400,
-        height=700
+        count = df_sub.shape[0]
+        df_sub_bar = df_sub.groupby(["date","fire origin"])["brightness"].mean().reset_index()
+        fig_bar = px.bar(
+            df_sub_bar,
+            x="date",
+            y="brightness",
+            labels={"brightness": "fire temperature (K)"},
+            color="fire origin",
+            barmode='group',
+            width=1400,
+            height=700
         )
-    return f"Fire instances: {count}", fig_bar
-
-# Getting map
-@app.callback(
-    Output('graph', 'figure'),
-    Input('fire-button', 'n_clicks'),
-    State('fire_origin_type', 'value'),
-    State('fire_time_type', 'value'),
-    State('map-theme', 'value'),
-    State('month-variable', 'value'),
-    State('year-variable', 'value'),
-    prevent_initial_call=True
-)
-def update_map(_, selected_category, selected_value, map_theme, selected_month, selected_year):
-    if selected_month and selected_year:
-        df_sub = df[(df['fire origin'].isin(selected_category)) & (df['fire time'].isin(selected_value)) & (df['month'].isin(selected_month)) & (df['year'].isin(selected_year))]
-    else:
-        df_sub = df[(df['fire origin'].isin(selected_category)) & (df['fire time'].isin(selected_value))]
-    print(len(df_sub))
-    fig_map = px.scatter_mapbox(
-        df_sub,
-        lat="latitude",
-        lon="longitude",
-        hover_data={"latitude": False, "longitude": False, "date": False},
-        color="brightness",
-        labels={"brightness": "fire temperature (K)"},
-        animation_frame="date",
-        mapbox_style=map_theme,
-        zoom=5,
-        width=1400,
-        height=700
+        fig_bar.update_layout(
+            legend=dict(
+                orientation='h',  
+                yanchor='bottom',
+                y=1.02,  
+                xanchor='right',
+                x=1
+            )
         )
-    fig_map.update_layout(title=f'Map for {selected_value} {selected_month} {selected_year}',
-                  mapbox_layers=[
-                      {
-                    "sourcetype": "image",
-                    "source": img,
-                    "coordinates": coordinates
-                      }
-                  ]
-    )
-    return fig_map
+        fig_map = px.density_mapbox(
+            df_sub,
+            lat="latitude",
+            lon="longitude",
+            hover_data={"latitude": False, "longitude": False, "date": False},
+            z="brightness",
+            labels={"brightness": "fire temperature (K)"},
+            animation_frame="date",
+            mapbox_style="carto-darkmatter",
+            zoom=5,
+            width=1400,
+            height=700
+        )
+        return f"Fire instances: {count}", fig_bar, fig_map, ''
+    else:
+        return {}, {}, {}, {}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
